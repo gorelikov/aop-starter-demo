815a81b79a55b016dc706fd2d614fc49da115d79
mgorelikov <mgorelikov@cian.ru>
2019-04-27 15:26:12 +0300

    chore: upgrade junit version

--- super-duper-starter/src/test/java/ru/alfa/starter/SuperControllerTest.java
+++ super-duper-starter/src/test/java/ru/alfa/starter/SuperControllerTest.java
@@ -1,28 +1,27 @@
 package ru.alfa.starter;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+@SpringJUnitConfig
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = RANDOM_PORT,
         classes = SuperControllerTest.TestFrontendControllerConfiguraiton.class
 )
-@AutoConfigureMockMvc
 public class SuperControllerTest {
     @Autowired MockMvc mvc;
 
